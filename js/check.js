b3a4b829b5067e5422af4c88dd15244f31b391c5
Nivedita Chopra <niveditc@andrew.cmu.edu>
2014-05-13 18:58:01 -0400

    Improved the UI

--- js/check.js
+++ js/check.js
@@ -8,6 +8,11 @@ function print_org(){
         var p1 = document.getElementById("org-check");
         var checkOrg = check_org(parsed);
         p1.innerText = (checkOrg.valid) ? "File is valid" : checkOrg.errors;
+        if(checkOrg.valid){
+            $("#org-header").removeClass("invalid").addClass("valid");
+        } else{
+            $("#org-header").removeClass("valid").addClass("invalid");
+        }
     };
     reader.readAsText(file);
 }
@@ -22,6 +27,11 @@ function print_loc(){
         var p1 = document.getElementById("loc-check");
         var checkLoc = check_loc(parsed);
         p1.innerText = (checkLoc.valid) ? "File is valid" : checkLoc.errors;
+        if(checkLoc.valid){
+            $("#loc-header").removeClass("invalid").addClass("valid");
+        } else{
+            $("#loc-header").removeClass("valid").addClass("invalid");
+        }
     };
     reader.readAsText(file);
 }
@@ -37,6 +47,11 @@ function print_service(){
         var checkService = check_service(parsed);
         p1.innerText = (checkService.valid) ? "File is valid" :
         checkService.errors;
+        if(checkService.valid){
+            $("#service-header").removeClass("invalid").addClass("valid");
+        } else{
+            $("#service-header").removeClass("valid").addClass("invalid");
+        }
     }
     reader.readAsText(file);
 }
@@ -52,6 +67,11 @@ function print_address(){
         var checkAddress = check_address(parsed);
         p1.innerText = (checkAddress.valid) ? "File is valid" :
         checkAddress.errors;
+        if(checkAddress.valid){
+            $("#address-header").removeClass("invalid").addClass("valid");
+        } else{
+            $("#address-header").removeClass("valid").addClass("invalid");
+        }
     }
     reader.readAsText(file);
 }
@@ -67,6 +87,13 @@ function print_contact(){
         var checkContact = check_contact(parsed);
         p1.innerText = (checkContact.valid) ? "File is valid" :
         checkContact.errors;
+        if(checkContact.valid){
+            $("#contact-header").removeClass("invalid neutral").addClass(
+                "valid");
+        } else{
+            $("#contact-header").removeClass("valid neutral").addClass(
+                "invalid");
+        }
     }
     reader.readAsText(file);
 }
@@ -82,6 +109,13 @@ function print_phone(){
         var checkPhone = check_phone(parsed);
         p1.innerText = (checkPhone.valid) ? "File is valid" :
         checkPhone.errors;
+        if(checkPhone.valid){
+            $("#phone-header").removeClass("invalid neutral").addClass(
+                "valid");
+        } else{
+            $("#phone-header").removeClass("valid neutral").addClass(
+                "invalid");
+        }
     }
     reader.readAsText(file);
 }
@@ -97,6 +131,13 @@ function print_internet(){
         var checkInternet = check_internet(parsed);
         p1.innerText = (checkInternet.valid) ? "File is valid" :
         checkInternet.errors;
+        if(checkInternet.valid){
+            $("#internet-header").removeClass("invalid neutral").addClass(
+                "valid");
+        } else{
+            $("#internet-header").removeClass("valid neutral").addClass(
+                "invalid");
+        }
     }
     reader.readAsText(file);
 }
@@ -112,6 +153,13 @@ function print_funding(){
         var checkFunding = check_funding(parsed);
         p1.innerText = (checkFunding.valid) ? "File is valid" :
         checkFunding.errors;
+        if(checkFunding.valid){
+            $("#funding-header").removeClass("invalid neutral").addClass(
+                "valid");
+        } else{
+            $("#funding-header").removeClass("valid neutral").addClass(
+                "invalid");
+        }
     }
     reader.readAsText(file);
 }

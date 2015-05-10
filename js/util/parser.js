6211a17e8a3d49ad2085cde5f51ab0717f9d389d
Nivedita Chopra <niveditc@andrew.cmu.edu>
2015-05-10 13:48:20 -0400

    Update code style (2 space indents)

--- js/util/parser.js
+++ js/util/parser.js
@@ -5,53 +5,52 @@
  * Remaining lines of the file will contain the rest of the code
  */
 
-function parse(doc){
-    //first line is the header
-    var lines = doc.split('\n');
-    var header = lines[0];
-    var categories = header.split(',');
-    var num_cat = categories.length;
-    var data = {};
-    for(var i = 0; i < categories.length; i++){
-        data[categories[i]] = [];
-    }
+function parse(doc) {
+  //first line is the header
+  var lines = doc.split('\n');
+  var header = lines[0];
+  var categories = header.split(',');
+  var num_cat = categories.length;
+  var data = {};
+  for (var i = 0; i < categories.length; i++) {
+    data[categories[i]] = [];
+  }
 
-    for(var k = 1; k < lines.length - 1; k++){
-        var line = lines[k];
-        var j = 0;
-        var ended = true;
-        var searching = '';
-        var word = '';
+  for (var k = 1; k < lines.length - 1; k++) {
+    var line = lines[k];
+    var j = 0;
+    var ended = true;
+    var searching = '';
+    var word = '';
 
-        for(var i = 0; i < line.length; i++){
-            if(ended){
-                if(line.charAt(i) != ','){
-                    ended = false;
-                    word = line.charAt(i);
-                    if(line.charAt(i) == '"'){
-
-                        if(line.charAt(i+1) == '['){
-                            searching = ']"';
-                            word = word + '[';
-                            i++;
-                        } else{
-                            searching = '"';
-                        }
-                    }
-                } else{
-                    j++;
-                }
-            } else{
-                if(line.substring(i, i + searching.length) == searching){
-                    word = word + searching;
-                    i = i + searching.length - 1;
-                    data[categories[j]].push(word);
-                    ended = true;
-                } else{
-                    word = word + line.charAt(i);
-                }
+    for (var i = 0; i < line.length; i++) {
+      if (ended) {
+        if (line.charAt(i) != ',') {
+          ended = false;
+          word = line.charAt(i);
+          if (line.charAt(i) == '"') {
+            if (line.charAt(i+1) == '[') {
+              searching = ']"';
+              word = word + '[';
+              i++;
+            } else {
+              searching = '"';
             }
+          }
+        } else {
+          j++;
         }
+      } else {
+        if (line.substring(i, i + searching.length) == searching) {
+          word = word + searching;
+          i = i + searching.length - 1;
+          data[categories[j]].push(word);
+          ended = true;
+        } else {
+          word = word + line.charAt(i);
+        }
+      }
     }
-    return data;
+  }
+  return data;
 }
--- js/util/types.js
+++ js/util/types.js
@@ -1,64 +1,64 @@
-function isValidString(s){
-    return (s.charAt(0) == '"') && (s.charAt(s.length - 1) == '"');
+function isValidString(s) {
+  return (s.charAt(0) == '"') && (s.charAt(s.length - 1) == '"');
 }
 
-function isValidNum(n){
-    var num = Number(n);
-    return !isNaN(num);
+function isValidNum(n) {
+  var num = Number(n);
+  return !isNaN(num);
 }
 
-function isValidDate(date){
-    var a = date.split('/');
-    var m = Number(a[0]);
-    var d = Number(a[1]);
-    var y = Number(a[2]);
-    if(isNaN(m) || isNaN(d) || isNaN(y)){
-        return false;
-    }
-    if(m < 1 || m > 12){
-        return false;
-    }
-    if(d < 1 || d > 31){
-        return false;
-    }
-    if(y < 0 || y > 3000){
-        //hardcoded coz code won't be used in y > 3000
-        return false;
-    }
-    return true;
+function isValidDate(date) {
+  var a = date.split('/');
+  var m = Number(a[0]);
+  var d = Number(a[1]);
+  var y = Number(a[2]);
+  if (isNaN(m) || isNaN(d) || isNaN(y)) {
+    return false;
+  }
+  if (m < 1 || m > 12) {
+    return false;
+  }
+  if (d < 1 || d > 31) {
+    return false;
+  }
+  if (y < 0 || y > 3000) {
+    //hardcoded coz code won't be used in y > 3000
+    return false;
+  }
+  return true;
 }
 
-function isValidStringList(data){
-    if(data == '"[]"') return true;
-    if(!isValidString(data)){
-        console.log("not string");
-        return false;
+function isValidStringList(data) {
+  if (data == '"[]"') return true;
+  if (!isValidString(data)) {
+    console.log("not string");
+    return false;
+  }
+  if (!(data.charAt(1) == '[' && data.charAt(data.length - 2) == ']')) {
+    console.log("not array");
+    return false;
+  }
+  var s = data.substring(2, data.length - 2);
+  var a = s.split(', ');
+  for (var i = 0; i < a.length; i++) {
+    if (!isValidString(a[i])) {
+      console.log(a[i] + "is not a valid string");
+      return false;
     }
-    if(!(data.charAt(1) == '[' && data.charAt(data.length - 2) == ']')){
-        console.log("not array");
-        return false;
-    }
-    var s = data.substring(2, data.length - 2);
-    var a = s.split(', ');
-    for(var i = 0; i < a.length; i++){
-        if(!isValidString(a[i])){
-            console.log(a[i] + "is not a valid string");
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
-function isValid(type, data){
-    if(type == 'string' || type == 'text' || type == 'comma-sep-strings'){
-        return isValidString(data);
-    } else if(type == 'int'){
-        return isValidNum(data);
-    } else if(type == 'date'){
-        return isValidDate(data);
-    } else if(type == 'string-list'){
-        return isValidStringList(data);
-    } else{
-        return false;
-    }
+function isValid(type, data) {
+  if (type == 'string' || type == 'text' || type == 'comma-sep-strings') {
+    return isValidString(data);
+  } else if (type == 'int') {
+    return isValidNum(data);
+  } else if (type == 'date') {
+    return isValidDate(data);
+  } else if (type == 'string-list') {
+    return isValidStringList(data);
+  } else {
+    return false;
+  }
 }

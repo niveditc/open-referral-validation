e015a94f4b12c80e5e949d9ce6d6bb627143e949
Nivedita Chopra <niveditc@andrew.cmu.edu>
2015-05-13 09:42:12 -0400

    More rigorous field checks + remove dead code

--- js/check_file.js
+++ js/check_file.js
@@ -5,7 +5,9 @@ function check_file(data, all_keys) {
   //Getting the required keys
   var reqd_keys = [];
   for (var i = 0; i < all_keys.length; i++) {
-    if (spec_fields[all_keys[i]].req === "required") {
+    if (!(all_keys[i] in spec_fields)) {
+      errors.push(all_keys[i] + " is not a valid field")
+    } else if (spec_fields[all_keys[i]].req === "required") {
       reqd_keys.push(all_keys[i]);
     }
   }
@@ -20,7 +22,7 @@ function check_file(data, all_keys) {
   //Checking that the field names present are valid
   for (var i = 0; i < data_keys.length; i++) {
     var f = data_keys[i];
-    if (all_keys.indexOf(f) == -1) {
+    if (all_keys.indexOf(f) == -1 || !(f in spec_fields)) {
       errors.push("Field '" + f + "' is invalid");
     } else {
       for (var j = 0; j < data[f].length; j++) {
@@ -41,36 +43,4 @@ function check_file(data, all_keys) {
   } else {
     return {valid : true, errors : ""};
   }
-}
-
-function check_org(data) {
-  return check_file(data, spec_files['organization']);
-}
-
-function check_loc(data) {
-  return check_file(data, spec_files['location']);
-}
-
-function check_service(data) {
-  return check_file(data, spec_files['service']);
-}
-
-function check_address(data) {
-  return check_file(data, spec_files['physical_address']);
-}
-
-function check_contact(data) {
-  return check_file(data, spec_files['contact']);
-}
-
-function check_phone(data) {
-  return check_file(data, spec_files['phone']);
-}
-
-function check_internet(data) {
-  return true;
-}
-
-function check_funding(data) {
-  return check_file(data, spec_files['funding']);
 }
